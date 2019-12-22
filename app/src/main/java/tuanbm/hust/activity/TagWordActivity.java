6dec355d942b9622bed99e94d4d565b32133d3f7
tuanbmhust <57906444+tuanbmhust@users.noreply.github.com>
2019-12-22 22:34:31 +0700

    second commit

---
+++ app/src/main/java/tuanbm/hust/activity/TagWordActivity.java
@@ -0,0 +1,14 @@
+package tuanbm.hust.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TagWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag_word);
+    }
+}
---
+++ app/src/main/java/tuanbm/hust/adapter/TagAdapter.java
@@ -0,0 +1,6 @@
+package tuanbm.hust.adapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TagAdapter extends RecyclerView.Adapter {
+}
